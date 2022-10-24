db165ea13a65b90177544a7ba259345af830d771
Olena <estasjuk@gmail.com>
2022-10-24 15:39:22 +0300

    add questionnaire validation

--- js/udemy-training.js
+++ js/udemy-training.js
@@ -8,12 +8,31 @@ const personalMovieDB = {
     privat: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", "");
-const b = prompt("На сколько оцените его?", "");
-const c = prompt("Один из последних просмотренных фильмов?", "");
-const d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i += 1) {
+    const a = prompt("Один из последних просмотренных фильмов?", "");
+    const b = prompt("На сколько оцените его?", "");
+    
+    if (a != null && b != null && a != "" && b != "" && a.length < 50 && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("Done");
+    }
+        else { 
+        console.log("Error");
+        i -= 1;
+        }
+}
+
+if (personalMovieDB.count < 10 && personalMovieDB.count > 0) {
+    console.log("Просмотрено довольно мало фильмов");
+}
+else if (personalMovieDB.count <= 30 && personalMovieDB.count >= 10) {
+    console.log("Вы - классический зритель");
+}
+else if (personalMovieDB.count > 30) {
+    console.log("Вы - киноман");
+}
+else console.log("Произошла ошибка"); 
+
 
 console.log(personalMovieDB);
